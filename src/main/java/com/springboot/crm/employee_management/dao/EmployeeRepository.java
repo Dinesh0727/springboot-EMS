38806afb181fc7dee3ba1de0d27f43751d583739
Dinesh <dineshttt2003@gmail.com>
2024-07-04 18:30:37 +0530

    Added the RepositoryRestSource to ensure no plural form issues and explored sorting, pagination and configuration

--- src/main/java/com/springboot/crm/employee_management/dao/EmployeeRepository.java
+++ src/main/java/com/springboot/crm/employee_management/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.springboot.crm.employee_management.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.springboot.crm.employee_management.Entity.Employee;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
 
 }
